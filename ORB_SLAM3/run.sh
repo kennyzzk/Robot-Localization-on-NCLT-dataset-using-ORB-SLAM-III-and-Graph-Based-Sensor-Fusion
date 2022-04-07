c15ee5578bcf35dc6dd9e1ae8a6a1ca6489f41c5
Zikun Zhou <kennyzzk@umich.edu>
2022-04-07 17:15:55 -0400

    Upload ORB-SLAM3 to the repo

---
+++ ORB_SLAM3/run.sh
@@ -0,0 +1,2 @@
+./Examples/Monocular-Inertial/mono_inertial_euroc ./Vocabulary/ORBvoc.txt ~/NCLT/NCLT.yaml ~/NCLT ~/NCLT/time_stamp.txt NCLT
+./Examples/Monocular/mono_euroc ./Vocabulary/ORBvoc.txt ~/NCLT/NCLT.yaml ~/NCLT ~/NCLT/time_stamp.txt NCLT
